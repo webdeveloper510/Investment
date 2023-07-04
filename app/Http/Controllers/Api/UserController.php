6c9080f6fad280484d8c4a17122a34176dcb8e34
Shreyacodenomad <shreyacodenomad@gmail.com>
2023-07-04 05:38:47 +0530

    changes

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class UserController extends Controller
+{
+    public $successStatus = 200;
+
+// ------------------------------------------------Investor Register-------------------------------------------------//
+
+    public function investorRegister(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+            'address' => 'required',
+            'contact' => 'required',
+            'gender' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+
+            return response()->json(['error'=>$validator->errors()], 401);
+        }
+
+        $input = $request->all();
+
+                $input['password'] = bcrypt($input['password']);
+                $user = User::create($input);
+                $success['token'] =  $user->createToken('MyLaravelApp')-> accessToken;
+                $success['name'] =  $user->name;
+
+        return response()->json(['success'=>$success], $this-> successStatus);
+    }
+
+// ------------------------------------------------Investor Login-------------------------------------------------//
+
+    public function investorLogin()
+    {
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyLaravelApp')-> accessToken;
+            $success['userId'] = $user->id;
+
+            return response()->json(['success' => $success], $this-> successStatus);
+        }
+        else{
+
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
+
+// ------------------------------------------------Investor Details-------------------------------------------------//
+
+    public function investorDetails()
+    {
+        $user = Auth::user();
+
+        return response()->json(['success' => $user], $this-> successStatus);
+    }
+}
